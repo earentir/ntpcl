1ee79a71b4f700092b45588eb0929c094d2c9650
earentir <earentir@gmail.com>
2025-10-02 16:49:58 +0300

    moved off mow.cli to cobra

--- go.mod
+++ go.mod
@@ -5,12 +5,13 @@ go 1.25.1
 require (
 	github.com/beevik/ntp v1.4.3
 	github.com/fatih/color v1.18.0
-	github.com/jawher/mow.cli v1.2.0
 	github.com/olekukonko/tablewriter v1.1.0
+	github.com/spf13/cobra v1.9.1
 )
 
 require (
 	github.com/clipperhouse/uax29/v2 v2.2.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.19 // indirect
@@ -18,6 +19,7 @@ require (
 	github.com/olekukonko/errors v1.1.0 // indirect
 	github.com/olekukonko/ll v0.1.1 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/spf13/pflag v1.0.6 // indirect
 	golang.org/x/net v0.44.0 // indirect
 	golang.org/x/sys v0.36.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,60 +3,74 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
 	"time"
 
 	"ntpcl/timeutils"
 
 	"github.com/beevik/ntp"
-	cli "github.com/jawher/mow.cli"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	app := cli.App("timeclient", "A simple time client to fetch and optionally set system time")
-	app.LongDesc = "A simple time client to fetch and optionally set system time. It can be used to query an NTP server, HTTP server, Daytime Protocol server, or Time Protocol server for the current time and set the system time to the retrieved time.\nhttps://github.com/earentir/ntpcl"
-	app.Version("v version", "0.4.19")
-
 	var (
-		ntpServer          = app.StringOpt("ntp-server", "europe.pool.ntp.org", "NTP server to query")
-		httpURL            = app.StringOpt("http-server", "", "URL to query for time from HTTP header")
-		daytimeServer      = app.StringOpt("daytime-server", "", "Daytime Protocol server to query")
-		timeProtocolServer = app.StringOpt("time-server", "", "Time Protocol server to query")
-		windowsTimeServer  = app.StringOpt("windows-time-server", "", "Windows Time Server to query")
-		setTime            = app.BoolOpt("set", false, "Set the system time")
-		highAccuracy       = app.BoolOpt("high-accuracy", false, "Use high accuracy mode (only with NTP)")
-		useSystemTools     = app.BoolOpt("system-tools", false, "Use system commands to set time instead of system calls")
+		ntpServer          string
+		httpURL            string
+		daytimeServer      string
+		timeProtocolServer string
+		windowsTimeServer  string
+		setTime            bool
+		highAccuracy       bool
+		useSystemTools     bool
 	)
 
-	app.Action = func() {
-		sources := []*string{httpURL, daytimeServer, timeProtocolServer, ntpServer, windowsTimeServer}
-		if countNonEmptySources(sources) > 1 {
-			log.Fatal("Only one time source can be selected.")
-		}
-
-		if *highAccuracy && *ntpServer == "" && *windowsTimeServer == "" {
-			log.Fatal("--high-accuracy can only be used with NTP.")
-		}
-
-		serverTime, roundTripTime, ntpResponse, server, err := fetchTime(httpURL, daytimeServer, timeProtocolServer, ntpServer, windowsTimeServer, *highAccuracy)
-		if err != nil {
-			log.Fatalf("Failed to fetch time: %v", err)
-		}
-
-		method := determineMethod(httpURL, daytimeServer, timeProtocolServer, ntpServer, windowsTimeServer)
-		timeutils.DisplayTimeInfo(method, serverTime, roundTripTime, server, ntpResponse)
-
-		if *setTime {
-			if err := timeutils.SetSystemTimeWrapper(serverTime, *useSystemTools); err != nil {
-				log.Fatalf("Failed to set system time: %v", err)
+	rootCmd := &cobra.Command{
+		Use:     "timeclient",
+		Short:   "A simple time client to fetch and optionally set system time",
+		Long:    "A simple time client to fetch and optionally set system time. It can be used to query an NTP server, HTTP server, Daytime Protocol server, or Time Protocol server for the current time and set the system time to the retrieved time.\nhttps://github.com/earentir/ntpcl",
+		Version: "0.4.19",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			sources := []*string{&httpURL, &daytimeServer, &timeProtocolServer, &ntpServer, &windowsTimeServer}
+			if countNonEmptySources(sources) > 1 {
+				return fmt.Errorf("Only one time source can be selected.")
 			}
-			fmt.Println("System time updated successfully")
-			printNewTimeInfo(serverTime)
-		}
+
+			if highAccuracy && ntpServer == "" && windowsTimeServer == "" {
+				return fmt.Errorf("--high-accuracy can only be used with NTP.")
+			}
+
+			serverTime, roundTripTime, ntpResponse, server, err := fetchTime(&httpURL, &daytimeServer, &timeProtocolServer, &ntpServer, &windowsTimeServer, highAccuracy)
+			if err != nil {
+				return fmt.Errorf("Failed to fetch time: %w", err)
+			}
+
+			method := determineMethod(&httpURL, &daytimeServer, &timeProtocolServer, &ntpServer, &windowsTimeServer)
+			timeutils.DisplayTimeInfo(method, serverTime, roundTripTime, server, ntpResponse)
+
+			if setTime {
+				if err := timeutils.SetSystemTimeWrapper(serverTime, useSystemTools); err != nil {
+					return fmt.Errorf("Failed to set system time: %w", err)
+				}
+				fmt.Println("System time updated successfully")
+				printNewTimeInfo(serverTime)
+			}
+
+			return nil
+		},
 	}
 
-	if err := app.Run(os.Args); err != nil {
-		log.Fatalf("Failed to run the app: %v", err)
+	rootCmd.Flags().StringVar(&ntpServer, "ntp-server", "europe.pool.ntp.org", "NTP server to query")
+	rootCmd.Flags().StringVar(&httpURL, "http-server", "", "URL to query for time from HTTP header")
+	rootCmd.Flags().StringVar(&daytimeServer, "daytime-server", "", "Daytime Protocol server to query")
+	rootCmd.Flags().StringVar(&timeProtocolServer, "time-server", "", "Time Protocol server to query")
+	rootCmd.Flags().StringVar(&windowsTimeServer, "windows-time-server", "", "Windows Time Server to query")
+	rootCmd.Flags().BoolVar(&setTime, "set", false, "Set the system time")
+	rootCmd.Flags().BoolVar(&highAccuracy, "high-accuracy", false, "Use high accuracy mode (only with NTP)")
+	rootCmd.Flags().BoolVar(&useSystemTools, "system-tools", false, "Use system commands to set time instead of system calls")
+	rootCmd.SilenceUsage = true
+	rootCmd.SetVersionTemplate("{{printf \"%s\" .Version}}\n")
+
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatalf("%v", err)
 	}
 }
 
